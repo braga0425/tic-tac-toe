e7b062e9f7dc42e116009bfbd3f9df9a128f5d53
Leonardo Braga <braga0425@gmail.com>
2023-04-17 22:36:31 -0300

    update main
    Incluindo a função play_tic_tac_toe.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def show_board(board):
-    print("  1 2 3")
+    print("\n  0  1  2")
     for i in range(3):
         print(i, end=" ")
         for j in range(3):
@@ -17,3 +17,37 @@ def winner_check(board, player):
     if board[0][2] == board[1][1] == board[2][0] == player:
         return True
     return False
+
+def play_tic_tac_toe():
+    board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    player = "X"
+
+    while True:
+        show_board(board)
+        jogada = input("\nJogador " + player + ", faça sua jogada (linha + coluna, com espaço entre os números): ")
+        linha, coluna = jogada.split()
+        linha = int(linha)
+        coluna = int(coluna)
+
+        if board[linha][coluna] != " ":
+            print("\nJogada inválida, tente novamente.\n")
+            continue
+
+        board[linha][coluna] = player
+
+        if winner_check(board, player):
+            show_board(board)
+            print("\nParabéns, jogador " + player + " venceu!")
+            return
+
+        if " " not in board[0] and " " not in board[1] and " " not in board[2]:
+            show_board(board)
+            print("\nEmpate!")
+            return
+
+        if player == "X":
+            player = "O"
+        else:
+            player = "X"
+
+play_tic_tac_toe()
