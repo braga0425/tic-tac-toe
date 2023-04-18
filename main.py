9eb8f6d58ad5762a1b507598ca51595b83e97c77
Leonardo Braga <braga0425@gmail.com>
2023-04-18 12:02:54 -0300

    update main
    Adicionando função para verificação de dígito e opção de jogar novamente.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 def show_board(board):
-    print("\n  0  1  2")
+    print("\n  0 1 2")
     for i in range(3):
         print(i, end=" ")
         for j in range(3):
             print(board[i][j], end=" ")
         print()
+
 
 def winner_check(board, player):
     for i in range(3):
@@ -18,22 +19,30 @@ def winner_check(board, player):
         return True
     return False
 
+
+def play_check(player):
+    while True:
+        play = input("\nJogador " + player + ", faça sua play (linha + coluna, com espaço entre os números): ")
+        lin, col = play.split()
+
+        if lin.isdigit() and col.isdigit() and int(lin) in range(3) and int(col) in range(3):
+            return int(lin), int(col)
+
+        print("\nJogada inválida, tente novamente.\n")
+
+
 def play_tic_tac_toe():
     board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
     player = "X"
 
     while True:
         show_board(board)
-        jogada = input("\nJogador " + player + ", faça sua jogada (linha + coluna, com espaço entre os números): ")
-        linha, coluna = jogada.split()
-        linha = int(linha)
-        coluna = int(coluna)
-
-        if board[linha][coluna] != " ":
+        lin, col = play_check(player)
+        if board[lin][col] != " ":
             print("\nJogada inválida, tente novamente.\n")
             continue
 
-        board[linha][coluna] = player
+        board[lin][col] = player
 
         if winner_check(board, player):
             show_board(board)
@@ -50,4 +59,9 @@ def play_tic_tac_toe():
         else:
             player = "X"
 
-play_tic_tac_toe()
+
+play_again = True
+while play_again:
+    play_tic_tac_toe()
+    response = input("\nDeseja jogar novamente? (s/n) ")
+    play_again = (response.lower() == "s")
